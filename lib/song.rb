a7023d46dcaf017f4e7f3e901803a6acce69941f
Marley <andrew.kigen@student.moringaschool.com>
2022-10-25 12:35:28 +0300

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,10 @@
 require 'pry'
-
+require_relative './Memorable'
 class Song
-  attr_accessor :name
-  attr_reader :artist
+  attr_accessor :name, :artist
 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   @@songs = []
 
   def initialize
@@ -11,23 +12,11 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+    @@songs.detect { |a| a.name == name }
   end
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
-  def artist=(artist)
-    @artist = artist
   end
 
   def to_param
